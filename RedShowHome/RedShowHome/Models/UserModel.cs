e108792ebae600a4538d84878800ab69f12b9795
zhengyf <zhengyf>
2017-03-28 17:26:47 +0800

    添加简单地图功能

--- RedShowHome/RedShowHome/Models/UserModel.cs
+++ RedShowHome/RedShowHome/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using RedShowHome.Models;
@@ -8,5 +9,10 @@ namespace RedShowHome.Models
 {
     public class UserModel
     {
+        RedShowHomeEntities rsEntities=new RedShowHomeEntities();
+
+        public void test()
+        {
+        }
     }
 }
